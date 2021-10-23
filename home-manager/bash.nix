5169fef46cfe0ad6ea3bc0669bf419179385d51a
Alexander Rezvov <alex@rezvov.ru>
2021-10-24 01:30:49 +0300

    Add wi function and some history settings

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -4,9 +4,16 @@
     enable = true;
     bashrcExtra = ''
       . ${pkgs.nixUnstable}/share/bash-completion/completions/nix
+
+      function wi() {
+        readlink -f $(type -p $1)
+      }
     '';
     shellAliases = {
       rr = "sudo nixos-rebuild switch --flake /home/alex/nixos#$(hostname) -L";
     };
+    historySize = 500000;
+    historyIgnore = [ "cd" "ls" ];
+
   };
 }
